fccdee2ab7f97c5480c4bcc68aed4f0a3fcabbcf
arafataft <arafatcd@gmail.com>
2023-05-20 13:52:08 +0600

    delete added

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const app =express();
-const cors= require('cors');
+const app = express();
+const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
@@ -31,53 +32,70 @@ async function run() {
 
 
     app.get('/allToys', async (req, res) => {
-        const cursor = toysInfo.find();
-        const result = await cursor.toArray();
-        res.send(result);
+      const cursor = toysInfo.find();
+      const result = await cursor.toArray();
+      res.send(result);
     })
 
     app.get('/myToys/:email', async (req, res) => {
-        const { email } = req.params;
-      
-        try {
-          const toys = await toysInfo.find({ postedBy: email }).toArray();
-          res.status(200).json(toys);
-        } catch (error) {
-          console.log('Error:', error);
-          res.status(500).json({ error: 'Internal server error' });
-        }
-      });
-      
+      const { email } = req.params;
+
+      try {
+        const toys = await toysInfo.find({ sellerEmail: email }).toArray();
+        res.status(200).json(toys);
+      } catch (error) {
+        console.log('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+
 
     app.post('/addToys', async (req, res) => {
-        try {
-          const toyData = req.body;
-          const result = await toysInfo.insertOne(toyData);
-            console.log(result);
-          res.status(201).json({ message: 'Toy added successfully' });
-        } catch (error) {
-          console.error(error);
-          res.status(500).json({ message: 'Error adding toy' });
+      try {
+        const toyData = req.body;
+        const result = await toysInfo.insertOne(toyData);
+        console.log(result);
+        res.status(201).json({ message: 'Toy added successfully' });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error adding toy' });
+      }
+    });
+
+    app.put('/updateToy/:id', async (req, res) => {
+      const { id } = req.params;
+      const updatedToy = req.body;
+
+      try {
+        const result = await toysInfo.updateOne({ _id: new ObjectId(id) }, { $set: updatedToy });
+        if (result.modifiedCount === 1) {
+          res.status(200).json({ message: 'Toy updated successfully' });
+        } else {
+          res.status(404).json({ error: 'Toy not found' });
         }
-      });
+      } catch (error) {
+        console.log('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
 
-      app.put('/updateToy/:id', async (req, res) => {
-  const { id } = req.params;
-  const updatedToy = req.body;
 
-  try {
-    const result = await toysInfo.updateOne({ _id: ObjectId(id) }, { $set: updatedToy });
-    if (result.modifiedCount === 1) {
-      res.status(200).json({ message: 'Toy updated successfully' });
-    } else {
-      res.status(404).json({ error: 'Toy not found' });
-    }
-  } catch (error) {
-    console.log('Error:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
-      
+    app.delete('/deleteToy/:id', async (req, res) => {
+      const { id } = req.params;
+
+      try {
+        const result = await toysInfo.deleteOne({ _id: new ObjectId(id) });
+        if (result.deletedCount === 1) {
+          res.status(200).json({ message: 'Toy deleted successfully' });
+        } else {
+          res.status(404).json({ error: 'Toy not found' });
+        }
+      } catch (error) {
+        console.log('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+      }
+    });
+
 
 
     // Send a ping to confirm a successful connection
@@ -91,10 +109,10 @@ async function run() {
 run().catch(console.dir);
 
 
-app.get('/',(req,res)=>{
-    res.send('server running')
+app.get('/', (req, res) => {
+  res.send('server running')
 })
 
-app.listen(port,()=>{
-    console.log(`server running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`server running on port: ${port}`);
 })
