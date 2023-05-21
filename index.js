41f9b60ccda0e9dc0051d29f090b43ff8d23a572
arafataft <arafatcd@gmail.com>
2023-05-21 13:30:01 +0600

    change api name

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await 
+    client.connect();
 
     const toysInfo = client.db('legoland').collection('toyInfo');
 
@@ -37,7 +38,7 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/myToys/:email', async (req, res) => {
+    app.get('/Toys/:email', async (req, res) => {
       const { email } = req.params;
       const { sortBy } = req.query;
     
